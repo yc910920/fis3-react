aeb0a922b350d27dcbdf9910173f0fb3b9e9b576
v_guofengwu <v_guofengwu@baidu.com>
2017-04-30 19:33:36 +0800

    mmm

--- fis-conf.js
+++ fis-conf.js
@@ -9,6 +9,9 @@ fis.set('project.files', ['*.html'])
 // 采用 commonjs 模块化方案
 fis.hook('commonjs', { tab: 4, baseUrl: '/src/', extList: ['.js', '.es6', '.jsx'] })
 
+// 采用 node_modules 依赖
+fis.hook('node_modules').unhook('components')
+
 // 开启模块化
 fis.match('/{src,node_modules}/**.{js,es6,jsx}', { isMod: true })
 
