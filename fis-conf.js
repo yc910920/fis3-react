0e326194d84c2fa0832d0a7cb85cb22990b01549
v_guofengwu <v_guofengwu@baidu.com>
2017-04-30 19:35:56 +0800

    mmm

--- fis-conf.js
+++ fis-conf.js
@@ -6,25 +6,36 @@
 // 按需加载
 fis.set('project.files', ['*.html'])
 
+
 // 采用 commonjs 模块化方案
-fis.hook('commonjs', { tab: 4, baseUrl: '/src/', extList: ['.js', '.es6', '.jsx'] })
+fis.hook('commonjs', {
+    tab: 4,
+    baseUrl: '/src/',
+    extList: ['.js', '.es6', '.jsx']
+})
+
 
 // 采用 node_modules 依赖
 fis.hook('node_modules').unhook('components')
 
+
 // 开启模块化
-fis.match('/{src,node_modules}/**.{js,es6,jsx}', { isMod: true })
+fis.match('/{src,node_modules}/**.{js,es6,jsx}', {
+    isMod: true
+})
+
 
 // 开启 babel
 fis.match('/src/**.{es6,jsx}', {
-  rExt: 'js',
-  parser: fis.plugin('babel-6.x')
+    rExt: 'js',
+    parser: fis.plugin('babel-6.x')
 })
+
 
 // 因为是纯前端项目，依赖不能自动被加载进来，所以这里需要借助一个 loader 来完成
 fis.match('::package', {
-  postpackager: fis.plugin('loader', {
-    useInlineMap: true,
-    resourcemapWhitespace: 4
-  })
+    postpackager: fis.plugin('loader', {
+        useInlineMap: true,
+        resourcemapWhitespace: 4
+    })
 })
