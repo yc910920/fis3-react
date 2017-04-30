73062745678d0ddd4412cf8663c5eb886315c60f
v_guofengwu <v_guofengwu@baidu.com>
2017-04-30 18:51:41 +0800

    mmm

--- fis-conf.js
+++ fis-conf.js
@@ -4,4 +4,10 @@
  */
 
 // 按需加载
-fis.set('project.files', ['*.html']);
+fis.set('project.files', ['*.html'])
+
+// 采用 commonjs 模块化方案
+fis.hook('commonjs', { tab: 4, baseUrl: '/src', extList: ['.js', '.es6', '.jsx'] })
+
+// 开启模块化
+fis.match('/{src,node_modules}/**.{js,es6,jsx}', { isMod: true })
