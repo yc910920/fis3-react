f159da0d1c472d47f44106cbb6c486088d925216
v_guofengwu <v_guofengwu@baidu.com>
2017-04-30 20:04:33 +0800

    mmm

--- fis-conf.js
+++ fis-conf.js
@@ -11,22 +11,8 @@ fis.hook('node_modules').unhook('components')
 
 // 采用 commonjs 模块化方案
 fis.hook('commonjs', {
-    tab: 4,
     baseUrl: '/src/',
     extList: ['.js', '.es6', '.jsx']
-})
-
-// 开启 babel
-fis.match('/src/**.{es6,jsx}', {
-    parser: fis.plugin('babel-5.x', {
-        sourceMaps: true
-    }),
-    rExt: 'js'
-})
-
-// 开启模块化
-fis.match('/{src,node_modules}/**.{js,es6,jsx}', {
-    isMod: true
 })
 
 // 压缩 html:js
@@ -34,10 +20,23 @@ fis.match('*.html:js', {
     optimizer: fis.plugin('uglify-js')
 })
 
+// 开启模块化
+fis.match('/{src,node_modules}/**.{js,es6,jsx}', {
+    isMod: true
+})
+
+// 开启 babel
+fis.match('/src/**.{es6,jsx}', {
+    rExt: 'js',
+    parser: fis.plugin('babel-5.x', {
+        sourceMaps: true
+    })
+})
+
 // 合并 node_modules 依赖
 fis.match('/node_modules/**.js', {
-    packTo: '/pkg/third.js',
-    optimizer: fis.plugin('uglify-js')
+    optimizer: fis.plugin('uglify-js'),
+    packTo: '/pkg/third.js'
 })
 
 // 因为是纯前端项目，依赖不能自动被加载进来，所以这里需要借助一个 loader 来完成
