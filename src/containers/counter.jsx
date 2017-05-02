02f36f830ead1c544de8894420d6d8635b320e89
v_guofengwu <v_guofengwu@baidu.com>
2017-05-02 14:24:56 +0800

    mmm

--- src/containers/counter.jsx
+++ src/containers/counter.jsx
@@ -5,7 +5,7 @@
  */
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Button, Progress } from 'antd';
+import { Button, Progress, message } from 'antd';
 
 
 
@@ -22,10 +22,12 @@ export default class Counter extends Component {
         let setState = this.setState.bind(this);
         return {
             handleAdd() {
+                if ( state.count === 0 ) return message.warn('别他妈的点了.');
                 setState({ count: ++state.count });
             },
 
             handleSubtract() {
+                if ( state.count === 100 ) return message.warn('别他妈的点了.');
                 setState({ count: --state.count });
             }
         };   
