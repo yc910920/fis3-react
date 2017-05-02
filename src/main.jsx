36a5209e07fe632f4d0a697a35ed7564d8793bf2
v_guofengwu <v_guofengwu@baidu.com>
2017-05-02 13:32:05 +0800

    mmm

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@
  * @file main.jsx
  * @email 99874405@qq.com
  */
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
 
 
 
-ReactDOM.render(
-    <div> hello </div>,
+render(
+    <div>hello</div>,
     document.getElementById('app')
 );
