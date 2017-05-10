cf64d55f7e72c0fbb9ef2d1664c661abdc057d2c
v_guofengwu <v_guofengwu@baidu.com>
2017-05-10 22:58:03 +0800

    mmm

--- src/components/time.jsx
+++ src/components/time.jsx
@@ -4,11 +4,7 @@
  */
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import { actionTypes } from '/src/constants';
-import { addAction, subtractAction, fetchInterfaceTestAction } from '/src/actions/counter';
-import { Button, Progress, message } from 'antd';
+import { Map } from 'immutable';
 
 
 
