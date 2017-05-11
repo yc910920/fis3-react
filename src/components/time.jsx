55732145ff6d337f2cf462c4c64f7503f00c0933
v_guofengwu <v_guofengwu@baidu.com>
2017-05-11 13:32:31 +0800

    mmm

--- src/components/time.jsx
+++ src/components/time.jsx
@@ -5,15 +5,15 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Map } from 'immutable';
-import { moment } from '/src/util';
+// import { moment } from '/src/util';
 
 
 
 export default class extends Component {
 
-    state = {
-        date: moment(Date.now()).format('YYYY-MM-DD hh:mm:ss a')
-    };
+    // state = {
+    //     date: moment(Date.now()).format('YYYY-MM-DD hh:mm:ss a')
+    // };
 
 
     shouldComponentUpdate(nextProps, nextState) {
