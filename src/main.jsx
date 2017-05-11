dbc9c220d4793a42d00a7a2e45022fad57e4455d
v_guofengwu <v_guofengwu@baidu.com>
2017-05-11 23:18:02 +0800

    mmm

--- src/main.jsx
+++ src/main.jsx
@@ -22,30 +22,26 @@ ReactDOM.render(
                 <div className="logo" />
                 <Menu mode="inline" theme="dark">
                     <SubMenu key="sub-01" title={<span><Icon type="appstore" /><span>menu 01</span></span>}>
-                        <Menu.Item key="sub-01-01"><Link to="/">root</Link></Menu.Item>
-                        <Menu.Item key="sub-01-02"><Link to="/home">home</Link></Menu.Item>
-                        <Menu.Item key="sub-01-03"><Link to="/about">about</Link></Menu.Item>
+                        <Menu.Item key="sub-01-01">1</Menu.Item>
+                        <Menu.Item key="sub-01-02">2</Menu.Item>
+                        <Menu.Item key="sub-01-03">3</Menu.Item>
                     </SubMenu>
                 </Menu>
             </Sider>
             <Layout>
                 <Header>此时你需要一个大大的微笑。并告诉自己，你可以的。</Header>
-                <Content>Content</Content>
+                <Content>
+                    <Router>
+                        <div>
+                            <Route exact path="/" component={Counter} />
+                            <Route path="/home" component={Home} />
+                            <Route path="/about" component={About} />
+                        </div>
+                    </Router>
+                </Content>
                 <Footer>蚂蚁金服体验技术部出品</Footer>
             </Layout>
         </Layout>
     </Provider>,
     document.getElementById('app')
 );
-/*<Router>
-            <div>
-                <ul>
-                    <li><Link to="/">root</Link></li>
-                    <li><Link to="/home">home</Link></li>
-                    <li><Link to="/about">about</Link></li>
-                </ul>
-                <Route exact path="/" component={Counter} />
-                <Route path="/home" component={Home} />
-                <Route path="/about" component={About} />
-            </div>
-        </Router>*/
