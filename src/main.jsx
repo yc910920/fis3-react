00190c10fcb6c478d5afdef8d4adfab619552224
v_guofengwu <v_guofengwu@baidu.com>
2017-05-11 20:19:11 +0800

    mmm

--- src/main.jsx
+++ src/main.jsx
@@ -16,6 +16,12 @@ ReactDOM.render(
     <Provider store={configureStore()}>
         <Router>
             <div>
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/counter">Counter</Link></li>
+                </ul>
+                <hr />
                 <Route path="/" component={Home} />
                 <Route path="/about" component={About} />
                 <Route path="/counter" component={Counter} />
