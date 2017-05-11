f4ea7d24f9cfdfc3757de1b669fb79ab21512bda
v_guofengwu <v_guofengwu@baidu.com>
2017-05-11 13:57:06 +0800

    mmm

--- src/components/time.jsx
+++ src/components/time.jsx
@@ -37,7 +37,7 @@ export default class extends Component {
 
 
     render() {
-        const { props } = this;
+        const { props, methods } = this;
         return (
             <div>
                 {props.currentTime}
