64756f98a9404d87416cf451740d04c6a435e3bd
v_guofengwu <v_guofengwu@baidu.com>
2017-05-11 23:05:51 +0800

    mmm

--- src/main.jsx
+++ src/main.jsx
@@ -21,16 +21,28 @@ ReactDOM.render(
             <Sider>
                 <div className="logo" />
                 <Menu mode="inline" theme="dark">
-                    <SubMenu key="sub_menu_01" title={<span><Icon type="setting" /><span>menu</span></span>}>
-                        <Menu.Item key="01">nav 01</Menu.Item>
-                        <Menu.Item key="02">nav 02</Menu.Item>
-                        <Menu.Item key="03">nav 03</Menu.Item>
-                        <Menu.Item key="04">nav 04</Menu.Item>
+                    <SubMenu key="sub-01" title={<span><Icon type="appstore" /><span>menu 01</span></span>}>
+                        <Menu.Item key="sub-01-01">nav 01</Menu.Item>
+                        <Menu.Item key="sub-01-02">nav 02</Menu.Item>
+                        <Menu.Item key="sub-01-03">nav 03</Menu.Item>
+                        <Menu.Item key="sub-01-04">nav 04</Menu.Item>
+                    </SubMenu>
+                    <SubMenu key="sub-02" title={<span><Icon type="setting" /><span>menu 02</span></span>}>
+                        <Menu.Item key="sub-02-01">nav 01</Menu.Item>
+                        <Menu.Item key="sub-02-02">nav 02</Menu.Item>
+                        <Menu.Item key="sub-02-03">nav 03</Menu.Item>
+                        <Menu.Item key="sub-02-04">nav 04</Menu.Item>
+                    </SubMenu>
+                    <SubMenu key="sub-03" title={<span><Icon type="mail" /><span>menu 03</span></span>}>
+                        <Menu.Item key="sub-03-01">nav 01</Menu.Item>
+                        <Menu.Item key="sub-03-02">nav 02</Menu.Item>
+                        <Menu.Item key="sub-03-03">nav 03</Menu.Item>
+                        <Menu.Item key="sub-03-04">nav 04</Menu.Item>
                     </SubMenu>
                 </Menu>
             </Sider>
             <Layout>
-                <Header>Header</Header>
+                <Header>此时你需要一个大大的微笑。并告诉自己，你可以的。</Header>
                 <Content>Content</Content>
                 <Footer>Footer</Footer>
             </Layout>
