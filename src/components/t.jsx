5330e21690e571105aed5e2c98b6a7a1cafaf154
v_guofengwu <v_guofengwu@baidu.com>
2017-05-11 10:08:27 +0800

    mmm

---
+++ src/components/t.jsx
@@ -0,0 +1,17 @@
+/**
+ * @module t
+ * @author 小虎牙
+ */
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Map } from 'immutable';
+
+
+
+export default function t() {
+    return (
+        <div>
+            {Date.now()}
+        </div>
+    );
+}
