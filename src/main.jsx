47ae9f3a2f09f674683b905642a5a9a4934b4570
v_guofengwu <v_guofengwu@baidu.com>
2017-05-11 22:48:02 +0800

    mmm

--- src/main.jsx
+++ src/main.jsx
@@ -19,7 +19,7 @@ ReactDOM.render(
         <Layout>
             <Sider>
                 <div className="logo" />
-                <Menu>
+                <Menu theme="dark">
                     <Menu.Item key="1">nav 1</Menu.Item>
                     <Menu.Item key="2">nav 2</Menu.Item>
                     <Menu.Item key="3">nav 3</Menu.Item>
