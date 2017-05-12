b3e8e9770affc6bf60f421f64516f6dbf3bc1f6c
v_guofengwu <v_guofengwu@baidu.com>
2017-05-12 20:15:21 +0800

    mmm

--- src/containers/counter.jsx
+++ src/containers/counter.jsx
@@ -50,8 +50,8 @@ export default class extends Component {
         return (
             <div className="module-wrap">
                 <div>
-                    <Button size="small" disabled={props.count === props.countMax} onClick={props.addAction}> 加加加 </Button>&nbsp;&nbsp;
-                    <Button size="small" disabled={props.count === props.countMin} onClick={props.subtractAction}> 剪剪剪 </Button>&nbsp;&nbsp;
+                    <Button size="small" disabled={props.count === props.countMax} onClick={props.addAction}> 加加 </Button>&nbsp;&nbsp;
+                    <Button size="small" disabled={props.count === props.countMin} onClick={props.subtractAction}> 剪剪 </Button>&nbsp;&nbsp;
                 </div>
                 <br />
                 <Progress type="circle" width={80} percent={props.count} />
