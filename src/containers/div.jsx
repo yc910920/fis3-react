3d4a08d02aeb84f539c2404431882030e96ffdc6
v_guofengwu <v_guofengwu@baidu.com>
2017-05-12 20:28:09 +0800

    mmm

--- src/containers/div.jsx
+++ src/containers/div.jsx
@@ -16,7 +16,111 @@ export default class extends Component {
     render() {
         return (
             <div className="module-wrap">
-                div
+                <div className="home-topics">
+                    <div className="more-actions">
+                        <a href="#" className="btn btn-block">
+                            更多专栏文章
+                        </a>
+                        <a href="#" className="btn btn-block">
+                            「发现」更多精彩
+                        </a>
+                    </div>
+                    <ul>
+                        <li>
+                            <a href="#" className="avatar">
+                                <img src="http://div.io/uploads/avatar/266_m.png" alt="jacobbubu" />
+                            </a>
+                            <h3>
+                                <a href="#">
+                                    Javascript 的前后端统一是个"笑话"吗?
+                                </a>
+                                <span className="author">
+                                    jacobbubu
+                                </span>
+                            </h3>
+                            <p>
+                                <a href="#">
+                                    最近发现知乎上有些人批评 Node.js，说 Javascript 的前后端统一是一个笑话。 “呵呵”。 所谓的统一当然是不可能的，前端自身都统一不了，何况前后端。不过，相当程度的重用是完全可行的。在这里我用一个实际的项目来说明，"i瑞士"。
+                                    该网站由瑞士国家旅游局立项、开发和维护，从新浪微博上不同的账号抓取......
+                                </a>
+                            </p>
+                        </li>
+                        <li>
+                            <a href="#" className="avatar">
+                                <img src="http://div.io/uploads/avatar/204_m.jpg" alt="MrZheng" />
+                            </a>
+                            <h3>
+                                <a href="#">
+                                    REM实战
+                                </a>
+                                <span className="author">
+                                    MrZheng
+                                </span>
+                            </h3>
+                            <p>
+                                <a href="#">
+                                    前两天有个朋友问起关于rem的一些问题，让我有些在意。因为随着CSS3的逐渐成熟，在优雅降级/渐进增强原则的保障下，很多CSS新特性已经不在是试用特性，而是在Web设计中得到广泛推行和应用！而CSS3新增的单位rem也在其列，当朋友问起时，我才意识到自己对这个单位也并不是很了解，趁着周末有些闲暇时光，不妨给自己的大脑充......
+                                </a>
+                            </p>
+                        </li>
+                        <li>
+                            <a href="#" className="avatar">
+                                <img src="http://div.io/uploads/avatar/2776_m.jpg" alt="wanghx" />
+                            </a>
+                            <h3>
+                                <a href="#">
+                                    仿造slither.io第二步：加个地图，加点吃的
+                                </a>
+                                <span className="author">
+                                    wanghx
+                                </span>
+                            </h3>
+                            <p>
+                                <a href="#">
+                                    前言 上一篇博文讲了如何造一条蛇，现在蛇有了，要让它自由的活动起来，就得有个地图啊，而且只能走也不行呀，还得有点吃的，所以还得加点食物，这一篇博文就来讲讲如何添加地图和食物。
+                                    预览效果 当前项目最新效果：http://whxaxes.github.io/slither/ （由于代码一直在更新，效果可能会比本文所述......
+                                </a>
+                            </p>
+                        </li>
+                        <li>
+                            <a href="#" className="avatar">
+                                <img src="http://div.io/uploads/avatar/128_m.jpg" alt="zenany" />
+                            </a>
+                            <h3>
+                                <a href="#">
+                                    FEX 技术周刊 - 2016.02.29
+                                </a>
+                                <span className="author">
+                                    zenany
+                                </span>
+                            </h3>
+                            <p>
+                                <a href="#">
+                                    原文： http://fex.baidu.com/blog/2016/02/fex-weekly-29/ 欢迎订阅 http://fex.baidu.com/feed.xml
+                                    业界会议 React.js Conf 2016 - 2.22&amp;2.23http://conf.reactjs.com/https:......
+                                </a>
+                            </p>
+                        </li>
+                        <li>
+                            <a href="#" className="avatar">
+                                <img src="http://div.io/uploads/avatar/212_m.JPG" alt="season" />
+                            </a>
+                            <h3>
+                                <a href="#">
+                                    Web性能优化与HTTP/2
+                                </a>
+                                <span className="author">
+                                    season
+                                </span>
+                            </h3>
+                            <p>
+                                <a href="#">
+                                    ...
+                                </a>
+                            </p>
+                        </li>
+                    </ul>
+                </div>
             </div>
         );
     }
