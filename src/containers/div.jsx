abbfa8806400a77ddbdb6491f8c49275f3472f15
v_guofengwu <v_guofengwu@baidu.com>
2017-05-12 20:20:22 +0800

    mmm

---
+++ src/containers/div.jsx
@@ -0,0 +1,23 @@
+/**
+ * @module div
+ * @author 小虎牙
+ * @desc DOME2
+ */
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+export default class extends Component {
+
+    /**
+     * @desc JSX
+     */
+    render() {
+        return (
+            <div className="module-wrap">
+                div
+            </div>
+        );
+    }
+}
