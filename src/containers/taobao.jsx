ac15c8312e2684ec49228ed018e4e7d4aec77c20
v_guofengwu <v_guofengwu@baidu.com>
2017-05-12 20:41:38 +0800

    mmm

---
+++ src/containers/taobao.jsx
@@ -0,0 +1,23 @@
+/**
+ * @module 淘宝网
+ * @author 小虎牙
+ * @desc DOME3
+ */
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+export default class extends Component {
+
+    /**
+     * @desc JSX
+     */
+    render() {
+        return (
+            <div className="module-wrap">
+                
+            </div>
+        );
+    }
+}
