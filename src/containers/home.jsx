82a7dd3a29ed047497c660e8663d51d98e932dd8
v_guofengwu <v_guofengwu@baidu.com>
2017-05-12 18:17:39 +0800

    mmm

---
+++ src/containers/home.jsx
@@ -0,0 +1,22 @@
+/**
+ * @module home
+ * @author 小虎牙
+ */
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+export default class extends Component {
+
+    /**
+     * @desc JSX
+     */
+    render() {
+        return (
+            <div>
+                123
+            </div>
+        );
+    }
+}
