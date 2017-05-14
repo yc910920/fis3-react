beb33be1e8cce0ab69d7b3e04de0bef707d92116
v_guofengwu <v_guofengwu@baidu.com>
2017-05-14 11:00:47 +0800

    mmm

--- src/containers/div.jsx
+++ src/containers/div.jsx
@@ -5,10 +5,22 @@
  */
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { Map, is } from 'immutable';
 
 
 
 export default class extends Component {
+
+    /**
+     * @method 组件是否渲染
+     * @return boolean
+     */
+    shouldComponentUpdate(nextProps, nextState) {
+        return (
+            !is(Map(nextProps), Map(this.props))
+        );
+    }
+
 
     /**
      * @desc JSX
